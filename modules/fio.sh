1035e6cc7317337cd5b600ca389d298f33456bd2
Simon A. F. Lund <os@safl.dk>
2021-12-20 16:20:21 +0100

    mod:fio: add a fio-wrapping module checking evars.
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ modules/fio.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# fio.sh - Wrapper for the Flexible I/O tester
+#
+# Functions:
+#
+# fio.env              - Setup default environment variables and check required ones
+# fio.run              - Translate ENV. VARS. into FIO_ARGS and run fio
+#
+## Variables EXPORTED by module:
+#
+# FIO_BIN               - Absolute PATH to fio binary (DEFAULT - see code)
+#
+# Variables EXPORTED by fio.run:
+#
+# FIO_ARGS              - Complete set of arguments for fio command
+#
+fio.env() {
+  if [[ ! -v FIO_FILENAME ]]; then
+    cij.err "fio.env: FIO_FILENAME must be set"
+    return 1
+  fi
+
+  FIO_BIN=${FIO_BIN:=/usr/local/bin/fio}; export FIO_BIN
+  FIO_ARGS=${FIO_ARGS:=}; export FIO_ARGS
+
+  return 0
+}
+
+fio.run() {
+  if ! fio.env; then
+    cij.err "fio.env failed"
+    return 1
+  fi
+
+  local _args="${FIO_ARGS}"
+
+  return cij.cmd "${FIO_BIN} ${_args}"
+}
+
